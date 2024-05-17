38436614037d984305abbc8af807d53da2113a9d
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-05-17 10:08:51 +0200

    dashboards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { FaGithub } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
 
 
 
 const Footer = () => {
   return (
-      <section className='flex flex-row justify-around h-20 mt-20'>
+      <section className='flex flex-row bg-red-700 justify-around items-center h-20'>
         <div className='flex flex-row'>
           <div>
         <img src='/Slogo.PNG' alt='footer' className='h-10'/>
@@ -21,6 +24,12 @@ const Footer = () => {
         </div>
         <div>
         <FaTwitter className='text-black '  />
+        </div>
+        <div>
+        <FaLinkedin className='text-black '  />
+        </div>
+        <div>
+        <FaInstagram className='text-black '  />
         </div>
         </div>
       </section>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './NavBar';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 const Layout = () => {
   return (
     <section className=' flex flex-col mx-5'>
       <NavBar />
       <Outlet />
+      {/* <Footer/> */}
     </section>
   );
 };
