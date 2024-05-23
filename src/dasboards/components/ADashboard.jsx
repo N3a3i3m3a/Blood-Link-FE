e2a192d645bbc38f528369a17bb7d9762779915a
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-05-23 15:00:59 +0200

    naviagating btn dasgboards

--- src/dasboards/components/ADashboard.jsx
+++ src/dasboards/components/ADashboard.jsx
@@ -7,17 +7,19 @@ import RecentDoners from '../components/RecentDoners';
 
 export default function ADashboard() {
   return (
-    <div className="w-full overflow-x-hidden p-4 max-w-7xl mx-auto">
-      <div className="flex flex-col gap-4">
-        <DashboardStatsGrid />
-        <div className="flex flex-row gap-4">
+    <div className="flex flex-col gap-4 w-full p-4">
+      <DashboardStatsGrid />
+      <div className="flex flex-row lg:flex-row w-fit gap-4">
+        
           <TransactionChart />
+        
+        
           <DonorProfilePieChart />
-        </div>
-        <div className="flex flex-row gap-4">
-          <RecentDoners />
-          {/* <PopularProducts /> */}
-        </div>
+        
+      </div>
+      <div className="w-full">
+        <RecentDoners />
+        {/* <PopularProducts /> */}
       </div>
     </div>
   );
